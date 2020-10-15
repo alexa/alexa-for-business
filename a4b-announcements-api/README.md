412c4f52db478e4d1cc07a7be430f6bda46ec354
Victor Rojo <48607816+vicrojo@users.noreply.github.com>
2020-10-15 15:30:40 -0700

    Code Update and README.md text update
    Added support for RoomARN and added JSDOC tags to document the code
    Updated the README.md text

--- a4b-announcements-api/README.md
+++ a4b-announcements-api/README.md
@@ -1,11 +1,11 @@
-# :rocket: Using the Amazon Alexa for Business (A4B) API to send announcements :rocket:
+## :rocket: Using the Amazon Alexa for Business (A4B) API to send announcements
 
-## Introduction
+### Introduction
 
-This sample code shows how to [Send Announcements](https://docs.aws.amazon.com/a4b/latest/ag/announcements.html) to rooms in A4B using the [JavaScript SDK](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/AlexaForBusiness.html#sendAnnouncement-property).
+This sample code shows how to [Send Announcements](https://docs.aws.amazon.com/a4b/latest/ag/announcements.html) to rooms in A4B using the [JavaScript SDK](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/AlexaForBusiness.html#sendAnnouncement-property). It uses passed in string messages. For the rooms, the script uses the Key RoomName, or Room ARN.
 
-It uses text messages, but can be extended to use SSML and / or Audio. For the rooms, I used the Key RoomName, but it can be switched to using the Room ARN, Profile ARN, or the Profile Name
+The message is case-insensitive and the Room name / Room ARN is case-sensitive.
 
-## Prerequisites
+### Prerequisites
 
-This code should be run with using AWS credentials with access granted to Alexa for Business
+This code should be run with using AWS credentials with AlexaForBusinessFullAccess.
--- a4b-announcements-api/index.js
+++ a4b-announcements-api/index.js
@@ -1,6 +1,9 @@
 var AWS = require("aws-sdk");
 var A4B = new AWS.AlexaForBusiness({ region: "us-east-1" });
 
+/**
+ * @description Return a unique ID
+ */
 function getToken() {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
         var r = (Math.random() * 16) | 0,
@@ -9,11 +12,31 @@ function getToken() {
     });
 }
 
+/**
+ * @description Send in string messages using the A4B sendAnnouncement API operation
+ * @param  {String} roomName=undefined This is a case-sensitive parameter to specify the room name
+ * @param  {String} roomARN=undefined This is a case-sensitive parameter to specify the room ARN
+ * @param  {String} message="Hello folks this is a default announcement" This is a case-insensitive parameter to specify the message to send
+ * @param  {integer} ttl=300
+ * @example <caption> Example of usage with Room Name</caption>
+ * sendAnnouncement("Room Name", undefined,"Please leave the room until it reaches it's max capacity!");
+ * @example <caption> Example of usage with Room ARN</caption>
+ * sendAnnouncement( undefined,"Room ARN","This room as reached MAX capacity and no one else can be admitted into this room");
+ */
 async function sendAnnouncement(
-    roomName,
+    roomName = undefined,
+    roomARN = undefined,
     message = "Hello folks, this is a default announcement",
     ttl = 300
 ) {
+    var roomKey = "RoomName";
+    var room = roomName;
+
+    if (!roomName && roomARN) {
+        roomKey = "RoomArn";
+        room = roomARN;
+    }
+
     var params = {
         ClientRequestToken: getToken(),
         Content: {
@@ -26,10 +49,9 @@ async function sendAnnouncement(
         },
         RoomFilters: [
             {
-                Key: "RoomName" /* required */,
-                Values: [/* required */ roomName],
+                Key: roomKey,
+                Values: [room],
             },
-            /* more items */
         ],
         TimeToLiveInSeconds: ttl,
     };
@@ -39,8 +61,3 @@ async function sendAnnouncement(
         else console.log(data); // successful response
     });
 }
-
-sendAnnouncement(
-    "war room",
-    "This room as reached MAX capacity and no one else can be admitted into this room"
-);
